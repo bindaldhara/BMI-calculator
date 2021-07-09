92906fcdcd7b0b40d23c6ad9863a06f3222cb610
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-07-09 21:37:48 +0530

    Delete BMI directory

--- BMI/script.js
+++
@@ -1,7 +0,0 @@
-let button=document.getElementById("button");
-button.addEventListener('click',function(){
-let weight=document.getElementById("weight").value;
-let height=document.getElementById("height").value;
-let finalbmi=(weight/(height*height))*10000;
-document.getElementById("BMI").value=finalbmi;
-});
